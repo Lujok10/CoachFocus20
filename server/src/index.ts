d63a45252fdfce6ba2913883a9ff22325a554f67
Joseph Kabaso <jspkabaso@gmail.com>
2026-06-08 23:35:45 -0400

    Debug task creation body

--- server/src/index.ts
+++ server/src/index.ts
@@ -492,6 +492,8 @@ app.post("/api/focus/start", async (req, res, next) => {
 app.post("/api/tasks", async (req, res, next) => {
   try {
     const userId = getRequestUserId(req);
+       console.log("CREATE TASK BODY:", req.body);
+
 
     res.json(
       await createTask(userId, req.body ?? {})
