b8ddd5ff2aba092f4a35f1fa55d00f13a12b09e0
Joseph Kabaso <jspkabaso@gmail.com>
2026-09-12 23:16:34 -0400

    Fix Clerk proxy request routing

--- api/clerk.ts
+++ api/clerk.ts
@@ -3,16 +3,6 @@ import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
-app.use((req, _res, next) => {
-  const path = typeof req.query.path === "string" ? req.query.path : "";
-
-  if (path) {
-    req.url = `/__clerk/${path}`;
-  }
-
-  next();
-});
-
 app.use(
   clerkMiddleware({
     frontendApiProxy: {
